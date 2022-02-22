5ee92d90c4cfd3617dc7036f2d8a904b8c641d91
Bo Rosales <bonirosales@gmail.com>
2022-02-22 08:37:18 -0600

    switched to using class in some areas of main

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -10,29 +10,23 @@ public class Main {
     }
 
     private static void performCalculations(String[] args) {
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
-
         MathEquation[] equations = new MathEquation[4];
         equations[0] = create(100.0d, 50.0d, 'd');
         equations[1] = create(25.0d, 92.0d, 'a');
         equations[2] = create(225.0d, 17.0d, 's');
         equations[3] = create(11.0d, 3.0d, 'm');
 
-        if (args.length == 0) {
-            for (int i = 0; i < opCodes.length; i++) {
-                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-            }
-            for (double currentResult : results)
-                System.out.println(currentResult);
-        } else if(args.length == 1 && args[0].equals("interactive"))
+        for(MathEquation equation : equations) {
+            equation.execute();
+            System.out.println("result = " + equation.result);
+        }
+
+/*        if(args.length == 1 && args[0].equals("interactive"))
             executeInteractively();
         else if(args.length == 3)
             handleCommandLine(args);
         else
-            System.out.println("Please provide an operation code and 2 numeric values.");
+            System.out.println("Please provide an operation code and 2 numeric values.");*/
     }
 
     private static MathEquation create(double leftVal, double rightVal, char opCode) {
@@ -59,7 +53,7 @@ public class Main {
         else {
             double leftVal = valueFromWord(parts[1]);
             double rightVal = valueFromWord(parts[2]);
-            double result = execute(opCode, leftVal, rightVal);
+            double result = 0; //execute(opCode, leftVal, rightVal);
             displayResult(opCode, leftVal, rightVal, result);
         }
     }
@@ -96,31 +90,8 @@ public class Main {
         char opCode = args[0].charAt(0);
         double leftVal = Double.parseDouble(args[1]);
         double rightVal = Double.parseDouble(args[2]);
-        double result = execute(opCode, leftVal, rightVal);
+        double result = 0; //execute(opCode, leftVal, rightVal);
         System.out.println(result);
-    }
-
-    static double execute(char opCode, double leftVal, double rightVal) {
-        double result;
-        switch (opCode) {
-            case 'a':
-                result = leftVal + rightVal;
-                break;
-            case 's':
-                result = leftVal - rightVal;
-                break;
-            case 'm':
-                result = leftVal * rightVal;
-                break;
-            case 'd':
-                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
-                break;
-            default:
-                System.out.println("Invalid opCode " + opCode);
-                result = 0.0d;
-                break;
-        }
-        return result;
     }
 
     static char opCodeFromString(String operationName) {
